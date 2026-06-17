1a7f914508d63168d15b8ba4923b589f033f710f
fesnguyen <33418293+fesnguyen@users.noreply.github.com>
2026-06-17 22:40:57 +0700

    Simplify router and argument extractor by tool_caller

--- tests/tool_test.py
+++ tests/tool_test.py
@@ -1,14 +1,32 @@
-from app.tools.schemas import WebSearchInput
+from app.agent.tool_caller import ToolCaller
+from app.llm.unsloth_vlm import UnslothVLM
+from app.tools.registry import ToolRegistry
+from app.tools.calculator import CalculatorTool
+from app.tools.file_reader import FileReaderTool
 from app.tools.web_search import WebSearchTool
 
-tool = WebSearchTool()
-
-results = tool.run(
-    WebSearchInput(
-        query="Qwen3-VL"
-    )
+model = UnslothVLM(
+    model_name="unsloth/Qwen3-VL-4B-Instruct-unsloth-bnb-4bit"
 )
 
-for result in results:
-    print(result)
-    print("=" * 80)
+registry = ToolRegistry()
+
+registry.register(
+    CalculatorTool()
+)
+
+registry.register(
+    FileReaderTool()
+)
+
+registry.register(
+    WebSearchTool()
+)
+
+tool_caller = ToolCaller(model, registry)
+
+tool_call = tool_caller.generate_tool_call(
+    "Search latest Qwen3 release"
+)
+
+print(tool_call)
